93505758988ac8c7d8f18a37f36fca89f61c1ecd
Fabio Caccamo <fabio.caccamo@gmail.com>
2016-11-08 12:18:35 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -10,15 +10,16 @@ setup(
     include_package_data=True,
     license='MIT License',
     version=__version__,
-    description='django-admin-interface is a modern flat admin interface customizable by the admin itself.',
+    description='django-admin-interface is a modern responsive flat admin interface customizable by the admin itself.',
     author='Fabio Caccamo',
     author_email='fabio.caccamo@gmail.com',
     url='https://github.com/fabiocaccamo/django-admin-interface',
     download_url='https://github.com/fabiocaccamo/django-admin-interface/archive/%s.tar.gz' % __version__,
-    keywords=['django', 'admin', 'interface', 'ui', 'flat', 'theme', 'custom'],
+    keywords=['django', 'admin', 'interface', 'responsive', 'flat', 'theme', 'custom', 'ui'],
     install_requires=[
         'django-colorfield==0.1.10',
-        'django-flat-theme==1.1.3'
+        'django-flat-theme==1.1.4',
+        'django-flat-responsive==1.0.0'
     ],
     classifiers=[]
 )
