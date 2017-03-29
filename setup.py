a6bdb2b36dd9008884f94b13cc1a59aed24fd35e
Fabio Caccamo <fabio.caccamo@gmail.com>
2017-03-29 16:22:09 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
+
 from setuptools import find_packages, setup
 
 exec(open('admin_interface/version.py').read())
 
-
 setup(
     name='django-admin-interface',
-    packages=find_packages(),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     include_package_data=True,
-    license='MIT License',
     version=__version__,
     description='django-admin-interface is a modern responsive flat admin interface customizable by the admin itself.',
     author='Fabio Caccamo',
@@ -16,11 +15,33 @@ setup(
     url='https://github.com/fabiocaccamo/django-admin-interface',
     download_url='https://github.com/fabiocaccamo/django-admin-interface/archive/%s.tar.gz' % __version__,
     keywords=['django', 'admin', 'interface', 'responsive', 'flat', 'theme', 'custom', 'ui'],
+    requires=['django(>=1.7)'],
     install_requires=[
         'django-colorfield==0.1.12',
         'django-flat-theme==1.1.4',
-        'django-flat-responsive==1.2.0'
+        'django-flat-responsive==1.3.0'
     ],
-    classifiers=[]
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    license='MIT',
+    test_suite='runtests.runtests'
 )
 
