70b4be757d671bc86876b4568632bb6fe6064001
Bruno Alla <bruno.alla@founders4schools.org.uk>
2017-06-09 15:12:00 +0100

    Fix a Django deprecation warning
    Replace deprecated assignment_tag:
    
    `python/lib/python3.5/site-packages/admin_interface/templatetags/admin_interface_tags.py:11: RemovedInDjango20Warning: assignment_tag() is deprecated. Use simple_tag() instead`

--- admin_interface/templatetags/admin_interface_tags.py
+++ admin_interface/templatetags/admin_interface_tags.py
@@ -8,7 +8,7 @@ from admin_interface.models import Theme
 register = template.Library()
 
 
-@register.assignment_tag(takes_context = True)
+@register.simple_tag(takes_context = True)
 def get_admin_interface_theme(context):
 
     theme = None
