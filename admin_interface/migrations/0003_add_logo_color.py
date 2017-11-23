41a5ba61c8a3ac1d19c506f43c4f650eb688472c
Fabio Caccamo <fabio.caccamo@gmail.com>
2017-11-23 12:08:36 +0100

    Fixed migrations pep8 compliance

--- admin_interface/migrations/0003_add_logo_color.py
+++ admin_interface/migrations/0003_add_logo_color.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 import colorfield.fields
+
 from django.db import migrations, models
 
 
@@ -15,11 +17,19 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='theme',
             name='logo_color',
-            field=colorfield.fields.ColorField(blank=True, default='#FFFFFF', help_text='#FFFFFF', max_length=10, verbose_name='logo color'),
+            field=colorfield.fields.ColorField(
+                blank=True,
+                default='#FFFFFF',
+                help_text='#FFFFFF',
+                max_length=10,
+                verbose_name='logo color'),
         ),
         migrations.AlterField(
             model_name='theme',
             name='logo',
-            field=models.FileField(blank=True, help_text='(leave blank to use the default Django logo)', upload_to='admin-interface/logo/'),
+            field=models.FileField(
+                blank=True,
+                help_text='(leave blank to use the default Django logo)',
+                upload_to='admin-interface/logo/'),
         ),
     ]
