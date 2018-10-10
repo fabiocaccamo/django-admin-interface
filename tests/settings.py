6a1897b95c279791b900f54ae8ebb938ad5390fd
Fabio Caccamo <fabio.caccamo@gmail.com>
2018-10-10 15:48:43 +0200

    Updated tests, tox and travis to support django 2.1 and python 3.7.

--- tests/settings.py
+++ tests/settings.py
@@ -19,14 +19,24 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
     'django.contrib.sessions',
 ]
 
-MIDDLEWARE_CLASSES = [
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-]
+if django.VERSION < (2, 0):
+    MIDDLEWARE_CLASSES = [
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware'
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+    ]
+else:
+    MIDDLEWARE = [
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+    ]
 
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -35,6 +45,7 @@ TEMPLATES = [{
     'OPTIONS': {
         'context_processors': [
             'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
         ]
     },
 },]
