18706356971adc1e966cdaefe975fe470f71a6fe
Fabio Caccamo <fabio.caccamo@gmail.com>
2019-02-06 16:22:27 +0100

    Removed extra white space. [ci skip]

--- tests/tests.py
+++ tests/tests.py
@@ -28,10 +28,10 @@ class AdminInterfaceTestCase(TestCase):
 
     def __test_active_theme(self):
         theme = Theme.get_active_theme()
-        print( theme )
+        print(theme)
         self.assertTrue(theme != None)
         self.assertTrue(theme.active)
-        self.assertEqual(Theme.objects.filter( active = True ).count(), 1);
+        self.assertEqual(Theme.objects.filter(active=True).count(), 1);
 
     def test_default_theme_created_if_no_themes(self):
         Theme.objects.all().delete()
@@ -50,47 +50,47 @@ class AdminInterfaceTestCase(TestCase):
 
     def test_default_theme_activated_after_update_if_no_active_themes(self):
         Theme.objects.all().delete()
-        Theme.objects.all().update( active = False )
+        Theme.objects.all().update(active=False)
         self.__test_active_theme()
 
     def test_default_theme_activated_after_update_if_multiple_active_themes(self):
         Theme.objects.all().delete()
-        theme_1 = Theme.objects.create( name = 'Custom 1', active = True )
-        theme_2 = Theme.objects.create( name = 'Custom 2', active = True )
-        theme_3 = Theme.objects.create( name = 'Custom 3', active = True )
-        Theme.objects.update( active = False )
-        Theme.objects.update( active = True )
+        theme_1 = Theme.objects.create(name='Custom 1', active=True)
+        theme_2 = Theme.objects.create(name='Custom 2', active=True)
+        theme_3 = Theme.objects.create(name='Custom 3', active=True)
+        Theme.objects.update(active=False)
+        Theme.objects.update(active=True)
         self.__test_active_theme()
 
     def test_default_theme_activated_on_active_theme_deleted(self):
         Theme.objects.all().delete()
-        theme_1 = Theme.objects.create( name = 'Custom 1', active = True )
-        theme_2 = Theme.objects.create( name = 'Custom 2', active = True )
-        theme_3 = Theme.objects.create( name = 'Custom 3', active = True )
-        Theme.objects.filter( pk = Theme.get_active_theme().pk ).delete()
+        theme_1 = Theme.objects.create(name='Custom 1', active=True)
+        theme_2 = Theme.objects.create(name='Custom 2', active=True)
+        theme_3 = Theme.objects.create(name='Custom 3', active=True)
+        Theme.objects.filter(pk=Theme.get_active_theme().pk).delete()
         self.__test_active_theme()
 
     def test_last_theme_activated_on_multiple_themes_created(self):
         Theme.objects.all().delete()
-        theme_1 = Theme.objects.create( name = 'Custom 1', active = True )
-        theme_2 = Theme.objects.create( name = 'Custom 2', active = True )
-        theme_3 = Theme.objects.create( name = 'Custom 3', active = True )
-        self.assertEqual( Theme.get_active_theme().pk, theme_3.pk )
+        theme_1 = Theme.objects.create(name='Custom 1', active=True)
+        theme_2 = Theme.objects.create(name='Custom 2', active=True)
+        theme_3 = Theme.objects.create(name='Custom 3', active=True)
+        self.assertEqual(Theme.get_active_theme().pk, theme_3.pk)
         self.__test_active_theme()
 
     def test_last_theme_activated_on_multiple_themes_activated(self):
         Theme.objects.all().delete()
-        theme_1 = Theme.objects.create( name = 'Custom 1', active = True )
-        theme_2 = Theme.objects.create( name = 'Custom 2', active = True )
-        theme_3 = Theme.objects.create( name = 'Custom 3', active = True )
-        theme_4 = Theme.objects.create( name = 'Custom 4', active = True )
-        theme_5 = Theme.objects.create( name = 'Custom 5', active = True )
-        themes = [ theme_1, theme_2, theme_3, theme_4, theme_5 ]
+        theme_1 = Theme.objects.create(name='Custom 1', active=True)
+        theme_2 = Theme.objects.create(name='Custom 2', active=True)
+        theme_3 = Theme.objects.create(name='Custom 3', active=True)
+        theme_4 = Theme.objects.create(name='Custom 4', active=True)
+        theme_5 = Theme.objects.create(name='Custom 5', active=True)
+        themes = [theme_1, theme_2, theme_3, theme_4, theme_5]
         for i in range(5):
             random.shuffle(themes)
             for theme in themes:
                 theme.set_active()
-                self.assertEqual( Theme.get_active_theme().pk, theme.pk )
+                self.assertEqual(Theme.get_active_theme().pk, theme.pk)
         self.__test_active_theme()
 
     def test_templatetags_get_theme(self):
@@ -128,5 +128,5 @@ class AdminInterfaceTestCase(TestCase):
 
     def test_repr(self):
         theme = Theme.get_active_theme()
-        self.assertEqual( '{0}'.format(theme), 'Django' )
+        self.assertEqual('{0}'.format(theme), 'Django')
 
