efb38d5825b788ca7f60a3cfd00bdf4b993851c4
Fabio Caccamo <fabio.caccamo@gmail.com>
2019-02-06 17:01:17 +0100

    Fixed #53 - Cancel button does not work

--- admin_interface/static/admin/js/cancel.js
+++ admin_interface/static/admin/js/cancel.js
@@ -1,24 +1,18 @@
-if (typeof(django) !== 'undefined' && typeof(django.jQuery) !== 'undefined')
-{
+if (typeof(django) !== 'undefined' && typeof(django.jQuery) !== 'undefined') {
     (function($) {
-
         'use strict';
-
         $(document).ready(function() {
-
             $('.cancel-link').click(function(e) {
                 e.preventDefault();
                 var parentWindow = window.parent;
-                if (parentWindow && typeof(parentWindow.dismissRelatedObjectModal) === 'function') {
+                if (parentWindow && typeof(parentWindow.dismissRelatedObjectModal) === 'function' && parentWindow !== window) {
                     parentWindow.dismissRelatedObjectModal();
                 } else {
                     // fallback to default behavior
-                    history.back();
+                    window.history.back();
                 }
                 return false;
             });
-
         });
-
     })(django.jQuery);
 }
