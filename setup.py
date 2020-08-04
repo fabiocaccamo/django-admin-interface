79dcc111da50ba7f012b50c3815645e02fe4923e
Fabio Caccamo <fabio.caccamo@gmail.com>
2020-08-04 17:38:33 +0200

    Added django 3.1 to CI.

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
