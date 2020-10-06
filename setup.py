e967d09c8b1406a8ad223971ee65cbf6392fb297
Fabio Caccamo <fabio.caccamo@gmail.com>
2020-10-06 10:59:09 +0200

    Added python 3.9 to tests.

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Build Tools',
     ],
     license='MIT',
