3431565f741d6ddd5db8abd6e66fb59917266237
Fabio Caccamo <fabio.caccamo@gmail.com>
2021-11-24 18:44:28 +0100

    Removed theme css field.

---
+++ admin_interface/migrations/0024_remove_theme_css.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('admin_interface', '0023_theme_foldable_apps'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='theme',
+            name='css',
+        ),
+    ]
