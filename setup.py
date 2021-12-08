1f690978bf0462d2de2f99f76a7085108929e886
Fabio Caccamo <fabio.caccamo@gmail.com>
2021-12-08 15:52:41 +0100

    Added django 4.0 to CI.

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
         'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
