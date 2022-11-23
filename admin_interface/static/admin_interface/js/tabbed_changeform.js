5a4231ca83277b4132ca4d65458236f833afba41
vaz <vmohan@lenbox.io>
2022-11-23 15:46:53 +0100

    Extract js to static folder

---
+++ admin_interface/static/admin_interface/js/tabbed_changeform.js
@@ -0,0 +1,14 @@
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
