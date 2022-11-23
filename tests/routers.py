51d112cd63ab179f21cf30b840151f063d7ee534
vaz <vmohan@lenbox.io>
2022-11-23 11:07:34 +0100

    Add db routing test case

---
+++ tests/routers.py
@@ -0,0 +1,45 @@
+DATABASE_APPS_MAPPING = {
+    'admin_interface': 'default',
+}
+
+
+class DatabaseAppsRouter(object):
+    """
+        arouter to control all database operations on models for different
+        databases.
+
+        in case an app is not set in DATABASE_APPS_MAPPING, the router
+        will fallback to the `default` database.
+
+        Settings example:
+
+        DATABASE_APPS_MAPPING = {'app1': 'db1', 'app2': 'db2'}
+    """
+
+    def __init__(self, db_map=DATABASE_APPS_MAPPING):
+        """
+        If routers is not specified, default to DATABASE_APPS_MAPPING
+        """
+        self.db_map = db_map
+
+    def db_for_read(self, model, **hints):
+        """Point all read operations to the specific database"""
+        if model._meta.app_label in self.db_map:
+            return self.db_map[model._meta.app_label]
+
+        return None
+
+    def db_for_write(self, model, **hints):
+        """Point all write operations to the specific database"""
+        if model._meta.app_label in self.db_map:
+            return self.db_map[model._meta.app_label]
+
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """Allow any relation between apps that use the same database"""
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """Make sure that apps only appear in the related database"""
+        return None
---
+++ tests/test_multidb.py
@@ -0,0 +1,26 @@
+from unittest import expectedFailure
+from django.test import TestCase
+from django.db.utils import ConnectionRouter
+
+from admin_interface.models import Theme
+from .routers import DatabaseAppsRouter
+
+class AdminInterfaceModelsWithDBRoutingTestCase(TestCase):
+    databases = ["replica"]
+
+    def test_standard_dbrouter(self):
+        router = DatabaseAppsRouter()
+        db_for_theme = router.db_for_read(Theme)
+        assert db_for_theme == "default"
+
+    def test_dbrouter_fetches_db(self):
+        DATABASE_APPS_MAPPING = {
+            'admin_interface': 'replica',
+        }
+        router = DatabaseAppsRouter(db_map=DATABASE_APPS_MAPPING)
+        db_for_theme = router.db_for_read(Theme)
+        assert db_for_theme == "replica"
+
+    @expectedFailure
+    def test_dbrouter_errors_when_fetching_from_default(self):
+        Theme.get_active_theme()
