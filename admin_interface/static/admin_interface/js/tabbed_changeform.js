a81e08a8550df41bde57c9b65f36a404b3bfc6eb
vaz <vmohan@lenbox.io>
2022-11-24 09:58:43 +0100

    use classList for updating classes

--- admin_interface/static/admin_interface/js/tabbed_changeform.js
+++ admin_interface/static/admin_interface/js/tabbed_changeform.js
@@ -7,8 +7,8 @@ function openTab(evt, tabName) {
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].classList.remove("active");
   }
   document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+  evt.currentTarget.classList.add("active");
 }
