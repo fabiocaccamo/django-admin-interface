7db456557d27f09f52fd02417fb1f3677b992710
vaz <vmohan@lenbox.io>
2022-11-30 15:20:51 +0100

    rename tag

--- admin_interface/templatetags/admin_interface_tags.py
+++ admin_interface/templatetags/admin_interface_tags.py
@@ -131,7 +131,7 @@ def admin_interface_filter_removal_link(changelist, list_filter):
 
 
 @simple_tag()
-def has_multiple_tabs(adminform, inline_forms):
+def admin_interface_use_changeform_tabs(adminform, inline_forms):
     theme = get_admin_interface_theme()
     has_fieldset_tabs = theme.show_fieldsets_as_tabs and len(adminform.fieldsets) > 1
     has_inline_tabs = theme.show_inlines_as_tabs and len(inline_forms) > 0
